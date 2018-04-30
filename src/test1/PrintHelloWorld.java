e44b8aa4644bba59329d4bfb35bab1049be067f6
Evangeline  Liu <evangelineliu@Evangelines-MBP.wv.cc.cmu.edu>
2018-04-30 18:12:34 -0400

    Calls print square

--- src/test1/PrintHelloWorld.java
+++ src/test1/PrintHelloWorld.java
@@ -4,6 +4,7 @@ public class PrintHelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("Let's print a square!");
+		PrintShapes.printSquare(5);
 	}
-	
 }
