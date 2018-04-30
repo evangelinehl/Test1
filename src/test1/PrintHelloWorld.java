80d82efee9aa38b88667de69d66eb34e7ce8dfc6
Evangeline  Liu <evangelineliu@Evangelines-MBP.wv.cc.cmu.edu>
2018-04-30 19:26:42 -0400

    comment

--- src/test1/PrintHelloWorld.java
+++ src/test1/PrintHelloWorld.java
@@ -2,6 +2,10 @@ package test1;
 
 public class PrintHelloWorld {
 
+	/**
+	 * Main method
+	 * @param args no args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Let's print a square!");
